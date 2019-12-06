5158ea90b8e3bf275cd398a2c509ab66e60d6db6
killianchappe <killian.chappe@gmail.com>
2019-12-06 10:25:40 +0100

    Création du projet - BackZoo - 06/12/2019

---
+++ src/test/java/fr/adaming/formation/projetZoo/ProjetZooApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.adaming.formation.projetZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
